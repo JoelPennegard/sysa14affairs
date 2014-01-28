6f17df453f37760fc4c17b132898daf0ea5e1416
ludvigtarland <ludvigtarland@gmail.com>
2014-01-28 17:25:06 +0100

    Lol

--- AffairsSystem/AffairsSystem/Program.cs
+++ AffairsSystem/AffairsSystem/Program.cs
@@ -8,6 +8,7 @@ namespace AffairsSystem
 {
     static class Program
     {
+        //Nu ändrade Ludde
         /// Ändring 17:15
         /// <summary>
         /// The main entry point for the application.
