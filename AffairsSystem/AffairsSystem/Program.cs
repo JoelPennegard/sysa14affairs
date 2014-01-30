68f693bb53697de0822f104a1d83d3dbc3ad8866
Storgren <robin@storgren.com>
2014-01-30 10:47:30 +0100

    haha

--- AffairsSystem/AffairsSystem/Program.cs
+++ AffairsSystem/AffairsSystem/Program.cs
@@ -8,12 +8,7 @@ namespace AffairsSystem
 {
     static class Program
     {
-<<<<<<< HEAD
-        /// Ändring 17:26
-=======
-        //Nu ändrade Ludde
         /// Ändring 17:15
->>>>>>> 6f17df453f37760fc4c17b132898daf0ea5e1416
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
