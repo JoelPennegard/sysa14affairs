ebfd878f677c798560aee8557bfbf975f95677d7
maxlilja <maxrogerlilja@gmail.com>
2014-01-30 12:28:04 +0100

    ska pulla

--- AffairsSystem/AffairsSystem/Program.cs
+++ AffairsSystem/AffairsSystem/Program.cs
@@ -22,7 +22,7 @@ namespace AffairsSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LogIn());
+            Application.Run(new Form1());
 
         }
     }
