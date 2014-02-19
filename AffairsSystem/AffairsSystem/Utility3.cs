7b4c54a0afe85933ec3d1d59203bbbecd761e9ad
JoelPennegård <joelpennegard@hotmail.com>
2014-02-19 11:13:41 +0100

    tog bort dubletter av using

--- AffairsSystem/AffairsSystem/Utility3.cs
+++ AffairsSystem/AffairsSystem/Utility3.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
-using System.Collections.Generic;
+
 using System.Web.Script.Serialization;
 using System.Net;
 using System.Windows.Forms;
