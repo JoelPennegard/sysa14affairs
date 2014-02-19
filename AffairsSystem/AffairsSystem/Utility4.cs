c69b4e61dc43654fb4bb51a43534ae7f3fa93b0e
JoelPennegård <joelpennegard@hotmail.com>
2014-02-19 10:58:55 +0100

    Utility Error Messages

--- AffairsSystem/AffairsSystem/Utility4.cs
+++ AffairsSystem/AffairsSystem/Utility4.cs
@@ -20,12 +20,23 @@ namespace AffairsSystem
             
             if (e is SqlException)
             {
-              return "SqlException";  
+              SqlException sqle = e as SqlException;
+              switch (sqle.Errors[0].Number)
+              {
+                  case 2627: // Primary key violation
+                      return "The product or salesperson is allready r egistered in the database, there can't be any duplicate identifiers (product number or Social security number)";
+                      
+                  case 53: //Connection
+                      return "There was a problem relating to the connection to the database";
+
+                  default:
+                      return "The database encountered a problem";
+              }
             }
 
             else if (e is WebException)
             {
-                return "WebException";
+                return "The applikation failed to establish connection with the web based currency API";
             }
             else
             {
